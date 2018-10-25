dbbb0d31d2fb321796d46f277425085053a9fc64
Jacob Miller <miller.jacob16@gmail.com>
2018-10-25 15:24:57 -0400

    pos.txt and POSTextFile added

--- src/midtermpackage/POSApp.java
+++ src/midtermpackage/POSApp.java
@@ -1,10 +1,14 @@
 package midtermpackage;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
+
+
 
 public class POSApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//Project Goals: Create POS JCB Toy Shop
 		//Midterm Project from Grand Circus by:
 		//Christa Stephens, Jacob Miller & Brian Eazer
@@ -16,9 +20,14 @@ public class POSApp {
 		Scanner scnr = new Scanner (System.in);
 		//validator - replace prompt to what we want to output to user
 		//then min & max is the amount we have in list
-		int choice = Validator.getInt(scnr, "Enter the toy by number.", 1, 12);
+		// int choice = Validator.getInt(scnr, "Enter the toy by number.", 1, 12);
 		
 		//System.out.println(choice); //testing to see if it works
+		List<Toy> toys = POSTextFile.readFile();
+		for (Toy t : toys) {
+			System.out.println(t.getName() + " " + t.getCategory() + " " + t.getPrice() + " " + t.getDescription());
+		}
+		//System.out.println(POSTextFile.readFile());
  
 	}
 
--- src/midtermpackage/POSTextFile.java
+++ src/midtermpackage/POSTextFile.java
@@ -1,6 +1,33 @@
 package midtermpackage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class POSTextFile {
+	private static Path filePath = Paths.get("pos.txt");
 	
+	static List<Toy> readFile() throws IOException{
+		try {
+			List<String> lines = Files.readAllLines(filePath);
+			List<Toy> toys = new ArrayList<>(); 
+			for(String line : lines) {
+				String[] parts = line.split("\t");
+				Toy toy = new Toy();
+				toy.setName(parts[0]);
+				toy.setCategory(parts[1]);
+				toy.setPrice(Double.parseDouble(parts[2]));
+				toy.setDescription(parts[3]);
+				toys.add(toy);
+			}
+			return toys;
+		} catch(IOException | ArrayIndexOutOfBoundsException ex){
+			System.out.println("No toys on the shelf!");
+			return new ArrayList<>();
+		}
+	}
 
 }
